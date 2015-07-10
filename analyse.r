fbdf57c85adc7deec9a0c6a17e75c5da18c24d5f
Guilherme Duarte Marmerola <gdmarmerola@gmail.com>
2015-07-10 15:16:23 -0300

    Update analyse.r

--- analyse.r
+++ analyse.r
@@ -1,16 +1,16 @@
-setwd("D:\\Projetos & Estudos\\Side Projects\\unicamp-salarios")
+setwd("unicamp-salarios")
 
 #Lendo a tabela processada do pdf
 wage_table = read.table("processed.txt", header = TRUE, sep = "\t") 
 
 rem_bruta = wage_table$Remuneracao.Bruta
 
-#DistribuiÁ„o de remuneraÁıes (bruto)
+#Distribui√ß√£o de remunera√ß√µes (bruto)
 hist(rem_bruta, breaks = 200, 
-     col = "black", main = "RemuneraÁ„o Bruta Unicamp",
-     ylab = "FrequÍncia", xlab = "Valor")
+     col = "black", main = "Remunera√ß√£o Bruta Unicamp",
+     ylab = "Frequ√™ncia", xlab = "Valor")
 
-#Maior (e menor) remuneraÁ„o Bruta e matrÌcula correspondente:
+#Maior (e menor) remunera√ß√£o Bruta e matr√≠cula correspondente:
 #maior
 print(max(rem_bruta))
 max_index = which(rem_bruta == max(rem_bruta))
@@ -20,20 +20,20 @@ print(min(rem_bruta))
 min_index = which(rem_bruta == min(rem_bruta))
 print(wage_table$Matricula[min_index])
 
-#Quantos est„o acima do teto:
+#Quantos est√£o acima do teto:
 over = which(rem_bruta > 21631.05)
 tot_over = length(over)
 print(tot_over)
 print(tot_over/length(wage_table$Matricula) * 100)
 
-#Soma dos sal·rios e parcela que est· acima do teto:
+#Soma dos sal√°rios e parcela que est√° acima do teto:
 #total
 print(sum(rem_bruta)) 
 print(sum(rem_bruta) * 13)
 #acima do teto
 print(sum(rem_bruta[over]))
 print(sum(rem_bruta[over]) * 13)
-#proporÁ„o:
+#propor√ß√£o:
 print(sum(rem_bruta[over])/sum(rem_bruta) * 100)
 
 
